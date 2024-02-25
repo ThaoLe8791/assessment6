889e2e380f4f36238a3bfbe78e5dbe1cf07ea04c
admin <admin@admins-MacBook-Pro.local>
2024-02-25 16:46:08 +0700

    added live server for duelDuo test

--- __tests__/duelDuo.test.js
+++ __tests__/duelDuo.test.js
@@ -1,7 +1,8 @@
 const { Builder, Browser, By, until } = require("selenium-webdriver");
 
 let driver;
-
+let liveServer = 'http://3.141.153.10'
+let localServer = "http://localhost:8000"
 beforeEach(async () => {
   driver = await new Builder().forBrowser(Browser.CHROME).build();
 });
@@ -12,17 +13,17 @@ afterEach(async () => {
 
 describe("Duel Duo tests", () => {
   test("page loads with title", async () => {
-    await driver.get("http://localhost:8000");
+    await driver.get(localServer);
     await driver.wait(until.titleIs("Duel Duo"), 1000);
   });
   test("clicking the Draw button displays the div with id = 'choices'", async () => {
-    await driver.get("http://localhost:8000");
+    await driver.get(localServer);
     await driver.findElement(By.id("draw")).click();
     let choicesDivEl = await driver.findElement(By.id("choices"));
     await driver.wait(until.elementIsVisible(choicesDivEl), 1000)
   });
   test("clicking an 'Add to Duo' button displays the div with id = 'player-duo'", async () => {
-    await driver.get("http://localhost:8000");
+    await driver.get(localServer);
     await driver.findElement(By.id("draw")).click();
     let choicesDivEl = await driver.findElement(By.id("choices"));
     await driver.wait(until.elementIsVisible(choicesDivEl), 1000)
